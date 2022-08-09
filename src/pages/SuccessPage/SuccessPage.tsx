90559bdbd20e3fc369fdab394b6f4227cfc9926d
Emanuel-Oliveira <101813364+eoliveiradev@users.noreply.github.com>
2022-08-09 00:00:38 -0300

    Featured OrderInfo

--- src/pages/SuccessPage/SuccessPage.tsx
+++ src/pages/SuccessPage/SuccessPage.tsx
@@ -1,16 +1,62 @@
-import { DeliveryInfo, SuccessPageContainer } from "./styles";
+import { DeliveryInfo, OrderInfo, SuccessPageContainer } from "./styles";
 import SuccessPageImage from "../../assets/images/SuccessPageImage.png";
+import { useContext } from "react";
+import { OrderDataContext } from "../../App";
 
-export function SuccessPage(){
-  return(
+export function SuccessPage() {
+  const {confirmedOrderData} = useContext(OrderDataContext)
+
+  return (
     <SuccessPageContainer>
       <h1>Uhu! Pedido confirmado</h1>
       <p>Agora é só aguardar que logo o café chegará até você</p>
       <DeliveryInfo>
-        <div className="deliveryInfo__container">
-            <div className="linear-gradient-border"/>
-            
+        <div className="linear-gradient-border">
+          <div className="deliveryInfo__container">
+            <OrderInfo>
+              <div className="icon__wrapper">
+
+              </div>
+              <div className="info__wrapper">
+                <p>
+                  Entraga em 
+                  <strong>
+                    {confirmedOrderData.address.street}, {confirmedOrderData.address.number}
+                  </strong>
+                  {confirmedOrderData.address.neighborhood} -
+                  {confirmedOrderData.address.city}, 
+                  {confirmedOrderData.address.state}
+                </p>
+              </div>
+            </OrderInfo>
+            <OrderInfo>
+              <div className="icon__wrapper">
+
+              </div>
+              <div className="info__wrapper">
+                <h1>Previsão de entrega</h1>
+                <strong>20 min - 30 min </strong>
+
+              </div>
+            </OrderInfo>
+            <OrderInfo>
+              <div className="icon__wrapper">
+
+              </div>
+              <div className="info__wrapper">
+                <h1>Pagamento na entrega</h1>
+                <strong>
+                  {confirmedOrderData.paymentMethod == "cash" && ("Dinheiro")}
+                  {confirmedOrderData.paymentMethod == "creditCard" && ("Cartão de Crédito")}
+                  {confirmedOrderData.paymentMethod == "debitCard" && ("Cartão de Débito")}
+                </strong>
+
+              </div>
+            </OrderInfo>
+
+          </div>
         </div>
+
         <img src={SuccessPageImage} />
       </DeliveryInfo>
     </SuccessPageContainer>
--- src/pages/SuccessPage/styles.ts
+++ src/pages/SuccessPage/styles.ts
@@ -1,49 +1,90 @@
 import styled from "styled-components";
 
 export const SuccessPageContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+
   width: 100%;
 
-  margin-top: ${props => props.theme["heights"]["header-height"]};
+  margin-top: 184px;
+
+  @media(max-width: 1200px){
+    align-items: center;
+    margin-top: 130px;
+  }
+
+  h1{
+    font-family: ${props => props.theme["fonts"]["header"]};
+    font-size: 2rem;
+    font-weight: 800;
+    color: ${props => props.theme["product-colors"]["yellow-dark"]};
+  }
+
+  p{
+    font-size: 1.25rem;
+    color: ${props => props.theme["base-colors"]["base-subtitle"]};
+  }
 
 `
 
 export const DeliveryInfo = styled.div`
   display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+
+  margin-top: 40px;
+
+  @media(max-width: 1200px){
+    flex-direction: column;
+    gap: 20px;
+  }
+
+  .linear-gradient-border{
+    padding: 1px;
+    border-radius: 6px 36px 6px; 
+    background: linear-gradient(to right bottom, #dbac2c, #ff7936, #ff326b, #f100b2, #8047f8);
+  }
 
   .deliveryInfo__container{
-    position: relative;
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
 
-    width: 100%;
+    width: 90vw;
     max-width: 446px;
-
     min-height: 190px;
 
-    padding: 40px;
+    padding: 40px 40px;
     background-color: ${props => props.theme["base-colors"]["background"]};
     
     border-radius: 6px 36px 6px; 
- 
-    .linear-gradient-border{
-      position: absolute;
-
-      height: calc(100% + 1px);
-      width: calc(100% + 1px);
-
-      border-radius: 6px 36px 6px; 
-
-      left: 50%;
-      top: 50%;
-
-      transform: translate(-50%, -50%);
-
-      background: linear-gradient(to right bottom, #dbac2c, #ff7936, #ff326b, #f100b2, #8047f8);
-
-      z-index: -1;
-    }
 
     @media(max-width: 480px) {
       padding: 40px 2vw;
     }
   }
+
+  img{
+    width: 100%;
+    max-width: 492px;
+
+    transition: 0.6s;
+
+    @media(min-width: 1200px){
+      &:hover{
+        transform: translateX(30px);
+      }
+    }
+  }
+
+`
+
+
+export const OrderInfo = styled.div`
+  display: flex;
+  gap: 12px;
+  
 
 `
